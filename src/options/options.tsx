98e3ecc88b12988f0f350e5ef605ff79825f77ae
Deepjyot Singh Kapoor <kapoordeepjyotsingh29@gmail.com>
2024-04-25 21:07:30 -0400

    first commit

---
+++ src/options/options.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const test  = <p>Hello World Option</p>  ;
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+
+ReactDOM.render(test, root);
